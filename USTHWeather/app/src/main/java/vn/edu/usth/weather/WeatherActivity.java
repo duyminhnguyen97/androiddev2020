15c3364c62a5d7ea295f521888ee97a1a37d8900
duyminhnguyen97 <ndminh1997@gmail.com>
2019-11-12 21:53:57 +0700

    Practical 2

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,45 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.os.Bundle;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("InfoTag","onCreate");
+        setContentView(R.layout.activity_weather);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("InfoTag", "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("InfoTag", "onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("InfoTag", "onPause");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("InfoTag", "onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("InfoTag", "onDestroy");
+    }
+}
