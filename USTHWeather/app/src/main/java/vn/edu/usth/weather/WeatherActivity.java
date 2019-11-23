4b2b7d99ee5d9f762daa24e2d6d6e2011110939e
duyminhnguyen97 <ndminh1997@gmail.com>
2019-11-23 10:19:01 +0700

    Practical 7

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,16 +1,54 @@
 package vn.edu.usth.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.util.Log;
 import android.os.Bundle;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+        private String titles[] = new String[] { "Hanoi", "Paris", "Toulouse" };
+
+        public HomeFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+        @Override
+        public Fragment getItem(int page) {
+            switch (page) {
+                case 0: return WeatherAndForecastFragment.newInstance(0,"Page 1");
+                case 1: return WeatherAndForecastFragment.newInstance(1,"Page 2");
+                case 2: return WeatherAndForecastFragment.newInstance(2, "Page 3");
+            }
+            return null;
+        }
+        @Override
+        public CharSequence getPageTitle(int page) {
+            return titles[page];
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         Log.i("InfoTag", "onCreate");
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(
+                getSupportFragmentManager());
+        ViewPager pager = findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
     }
 
     @Override
